e81fd9216229c414eb0e78aa413ebec69c977a61
1409070209 <1142908626@qq.com>
2017-05-29 16:45:50 +0800

    第二次更新

--- js/getdata.js
+++ js/getdata.js
@@ -1,3 +1,16 @@
+var card = $('.photo');
+function select(myFriend) {
+	for (var i = 0; i < card.length; i++) {
+		var thisCaption = card.eq(i).find('.caption');
+		if (thisCaption.html() == myFriend) {
+			turn(card[i])
+		}
+	}
+}
 $(function () {
-	
+	$('.select-click').on('click', function() {
+		var serchMes = $('.select-input').val();
+		select(serchMes)
+		return false;
+	});
 })
